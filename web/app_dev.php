270f250472715520d0cc49e57ad9363d49baa618
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-07-24 20:47:51 +0200

    remove use of class cache

--- web/app_dev.php
+++ web/app_dev.php
@@ -16,5 +16,4 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
