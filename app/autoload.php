173c572039939791ea6c2457bf6be712c3a0e740
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-08-18 18:22:18 +0200

    fixed PHPCR ODM annotations

--- app/autoload.php
+++ app/autoload.php
@@ -55,6 +55,7 @@ AnnotationRegistry::registerLoader(function($class) use ($loader) {
     return class_exists($class, false);
 });
 AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+AnnotationRegistry::registerFile(__DIR__.'/../vendor/symfony-cmf/vendor/doctrine-phpcr-odm/lib/Doctrine/ODM/PHPCR/Mapping/Annotations/DoctrineAnnotations.php');
 
 // Swiftmailer needs a special autoloader to allow
 // the lazy loading of the init file (which is expensive)
