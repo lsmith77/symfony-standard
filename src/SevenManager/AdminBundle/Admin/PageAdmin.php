d75a6961116a32a48e2b7eaacf5030e1cf1c8208
7rin0 <l.severino@c2is.fr>
2015-10-14 15:13:21 +0200

    Seven manager - Content Types BO/FO

---
+++ src/SevenManager/AdminBundle/Admin/PageAdmin.php
@@ -0,0 +1,78 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: lseverino
+     * Date: 13/10/15
+     * Time: 07:42
+     */
+
+    namespace SevenManager\AdminBundle\Admin;
+
+    use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
+    use Sonata\AdminBundle\Datagrid\DatagridMapper;
+    use Sonata\AdminBundle\Datagrid\ListMapper;
+    use Sonata\AdminBundle\Form\FormMapper;
+
+    class PageAdmin extends Admin
+    {
+        protected function configureListFields(ListMapper $listMapper)
+        {
+            $listMapper
+                ->addIdentifier('title', 'text')
+                ->addIdentifier('name', 'text')
+                ->addIdentifier('path', 'text')
+                ->addIdentifier('lang', 'text')
+            ;
+        }
+
+        /**
+         * @param FormMapper $formMapper
+         */
+        protected function configureFormFields(FormMapper $formMapper)
+        {
+            // Define Admin fields
+            $formMapper
+                ->with('seven_manager.admin.pages.homepage.title')
+                ->add('title', 'text')
+                ->add('subtitle', 'text', array('required' => false))
+                ->add('name', 'text', array('required' => false))
+                ->add('content', 'textarea')
+                ->setHelps(array(
+                    'title' => 'seven_manager.admin.fields.title.helper',
+                    'subtitle' => 'seven_manager.admin.fields.subtitle.helper',
+                    'name' => 'seven_manager.admin.fields.name.helper',
+                    'content' => 'seven_manager.admin.fields.content.helper',
+                ))
+                ->end();
+        }
+
+        /**
+         * @param mixed $document
+         * @return $this
+         */
+        public function prePersist($document)
+        {
+            $parent = $this->getModelManager()->find(null, '/seven-manager/page');
+            $document->setParentDocument($parent);
+
+            return $this;
+        }
+
+        /**
+         * @param DatagridMapper $datagridMapper
+         */
+        protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+        {
+            $datagridMapper
+                ->add('title', 'doctrine_phpcr_string')
+                ->add('name',  'doctrine_phpcr_nodename');
+        }
+
+        /**
+         * @return array
+         */
+        public function getExportFormats()
+        {
+            return array();
+        }
+    }
---
+++ src/SevenManager/ContentBundle/Document/Page.php
@@ -0,0 +1,96 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: lseverino
+     * Date: 13/10/15
+     * Time: 07:42
+     */
+
+    namespace SevenManager\ContentBundle\Document;
+
+    use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
+    use Symfony\Cmf\Component\Routing\RouteReferrersReadInterface;
+
+    /**
+     * @PHPCR\Document(referenceable=true)
+     */
+    class Page implements RouteReferrersReadInterface
+    {
+        use SharedProperties;
+
+        /**
+         * @PHPCR\String(type="string", nullable=true)
+         */
+        protected $name;
+
+        /**
+         * @PHPCR\String(type="string", nullable=true)
+         */
+        protected $label;
+
+        /**
+         * @PHPCR\String(type="string", nullable=true)
+         */
+        protected $subtitle;
+
+        /**
+         * @return mixed
+         */
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        /**
+         * @param $name
+         *
+         * @return $this
+         */
+        public function setName($name)
+        {
+            $this->name = $name;
+
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getLabel()
+        {
+            return $this->label;
+        }
+
+        /**
+         * @param $label
+         *
+         * @return $this
+         */
+        public function setLabel($label)
+        {
+            $this->label = $label;
+
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getSubtitle()
+        {
+            return $this->label;
+        }
+
+        /**
+         * @param $subtitle
+         *
+         * @return $this
+         */
+        public function setSubtitle($subtitle)
+        {
+            $this->label = $subtitle;
+
+            return $this;
+        }
+
+    }
